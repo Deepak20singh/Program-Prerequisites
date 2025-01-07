8691b7181f33c86413fd46467ae8730180607eff
Deepak <singh017deepak@gmail.com>
2025-01-08 01:10:58 +0530

    Add Two Numbers

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class addition{
+public static void main(String args[]){
+int k,i;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the 1st number:-");
+k=sc.nextInt();
+System.out.println();
+System.out.print("Enter the 2nd number:-");
+i=sc.nextInt();
+System.out.println();
+
+System.out.println("the submission of "+k+" and "+i+" is "+(k+i));
+}
+}
